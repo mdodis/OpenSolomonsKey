a08293b880bb32490e7e5bddc8c32e31294b5cc7
mdodis <michaeldodisgr@gmail.com>
2020-08-30 09:07:27 +0300

    match editor build script on linux

--- build_editor.sh
+++ build_editor.sh
@@ -3,7 +3,8 @@
 TIMEFORMAT=%R
 
 code="$PWD/editor/"
-opts="-pthread -I$code/imgui/ -I./incl/ -Wno-conversion-null"
+incl="$PWD/src/"
+opts="-pthread -I$incl/ -I$code/imgui/ -I./incl/ -Wno-conversion-null"
 libs="-lGL -lGLU -lsfml-graphics -lsfml-window -lsfml-system"
 SOURCES="$code/imgui/imgui.cpp $code/imgui/imgui_draw.cpp $code/imgui/imgui_widgets.cpp $code/imgui/imgui-SFML.cpp $code/imgui/imgui_demo.cpp $code/editor.cpp"
 cd build
